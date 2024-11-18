2b82b65b29e29a862433e3c50f2084aab1b6fd00
Mikhail <mikhail@Mikhails-MacBook-Pro.local>
2024-11-17 20:04:57 -0800

    V1 - Structured OpenAI Output API

---
+++ ai.py
@@ -0,0 +1,85 @@
+import os
+from openai import OpenAI
+from typing import List
+from loguru import logger
+from sqlalchemy import create_engine, text
+from datetime import date
+
+from constants import CaseAnalysisSchema
+
+
+def get_openai_client():
+    return OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+def format_messages(
+    system_prompt: str,
+    user_prompt: str,
+    image_urls: List[str] = None
+):
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": user_prompt}
+            ]
+        }
+    ]
+
+    if image_urls:
+        # Add image URLs to the user's content list
+        messages[1]["content"].extend([
+            {"type": "image_url", "image_url": {"url": url}} 
+            for url in image_urls
+        ])
+
+    return messages
+
+
+def call_openai_structured(client: OpenAI, messages: List[dict]) -> CaseAnalysisSchema:
+    response = client.beta.chat.completions.parse(
+        model="gpt-4o-2024-08-06",
+        messages=messages,
+        response_format=CaseAnalysisSchema,
+    )
+    logger.info(f"Total tokens: {response.usage.total_tokens}")
+    return response.choices[0].message.parsed
+
+
+def store_case_analysis(
+    title: str,
+    image_urls: List[str],
+    case_analysis: CaseAnalysisSchema
+    ):
+    query = text("""
+        INSERT INTO viet_cases (
+            title,
+            images,
+            summary,
+            keypoints,
+            translations,
+            created_at
+        ) VALUES (
+            :title,
+            CAST(:images AS JSONB),
+            :summary,
+            CAST(:keypoints AS JSONB),
+            CAST(:translations AS JSONB),
+            :created_at
+        )
+        RETURNING id
+    """)
+    # Convert case analysis to a dict format
+    params = {
+        "title": title,
+        "images": image_urls,
+        "summary": case_analysis.summary,
+        "keypoints": case_analysis.key_points,
+        "translations": case_analysis.translations,
+        "created_at": date.today().isoformat()
+    }
+    db_url = "postgresql://retool:ClBAzE3tx1LH@ep-restless-butterfly-a65877e9.us-west-2.retooldb.com/retool?sslmode=require"
+    with create_engine(db_url).connect() as connection:
+        result = connection.execute(query, params)
+        return result.fetchone()[0]
---
+++ constants.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field
+from typing import List, Literal
+
+from models import RequestConfig
+
+
+SYSTEM_PROMPT = lambda config: f"""
+You are a vietnamese to english translator given a series of screenshots of messages. It is often that you will find the messages written with informal slang or its messages are out of order in context. Sometimes you know some context about the extracted messages to give you a further instructions.
+
+Author: {config.single_author} (When true, give a summary focusing on the single author. Otherwise, give a summary focusing on the conversation as a whole.)
+Username: {config.author_mapping} (A mapping of author to their username in the conversation. If not provided, the author will be the username of the person sending the message.)
+
+"""
+
+class Translation(BaseModel):
+    index: int = Field(description="The index of the screenshot")
+    vietnamese_text: str = Field(description="The original text to be translated from vietnamese to english extracted from the screenshot")
+    english_text: str = Field(description="The translated text in English corresponding to the original text")
+    confidence: Literal['high', 'medium', 'low'] = Field(description="The confidence in the translation's accuracy")
+
+class CaseAnalysisSchema(BaseModel):
+    translations: List[Translation] = Field(description="A list of translations of the conversation in english")
+    summary: str = Field(description="A detailed summary of the conversation in english, highlighting key takeaways, lessons learned, and any other important information. Also share any additional context that you think is important. Briefly also reference screenshot numbers to help direct readers, especially non-vietnamese speakers, to note-worthy parts of the conversation. An example would be: 'Screenshot 4 and 5 are great to read in detail because they contain the most important information about the case.'")
+    key_points: str = Field(description="A list of key points of the conversation in english, highlighting the most important information written in markdown format.")
+    
